f9e7e245b2c96dea0f9828084e026448595399b6
tomgalle <admin@a23-0-3-10.deploy.static.akamaitechnologies.com>
2014-09-23 18:04:25 -0400

    version 03

--- js/script.js
+++ js/script.js
@@ -8,27 +8,38 @@ $(function(){
     var logo = $(".logo");
     var arrow = $(".arrow")
 
-
     top.css ({ height: windowheight });
     logo.css({ "padding-top": halfpage });
     // arrow.css({ bo: windowheight })
 
 
 
-    $(window).on("scroll", function(){
-
-    	var st = $(document).scrollTop();
-    	var sc = (-0.9/500) * st + 1
-    	var scstr = 'scale('+ sc + ')'
-    	console.log(sc)
-
-    	logo.css({
-    		// 'webkit-transform' : 'scale(0.5)'
-    		'webkit-transform' : scstr
-    	});
 
 
-    });
+
+    docHeight = document.documentElement.offsetHeight;
+    console.log(docHeight);
+
+	  
+	  window.addEventListener( 'scroll', function() {
+	        // normalize scroll position as percentage
+	    var scrolled = window.scrollY / ( docHeight - window.innerHeight ),
+	        transformValue = 'scale('+scrolled+')';
+
+	    logo.css({
+
+
+	     })
+
+	    logo.style.WebkitTransform = transformValue;
+	    logo.style.MozTransform = transformValue;
+	    logo.style.OTransform = transformValue;
+	    logo.style.transform = transformValue;
+	    
+	  }, false);
+	  
+
+
 
 
  //   	console.log($(document).scrollTop());
@@ -39,6 +50,25 @@ $(function(){
 
  //    	logo.width(sw).height(sh);
 	// });
+
+
+
+
+
+	   //  $(window).on("scroll", function(){
+
+    // 	var st = $(document).scrollTop();
+    // 	var sc = (-0.9/500) * st + 1
+    // 	var scstr = 'scale('+ sc + ')'
+    // 	console.log(sc)
+
+    // 	logo.css({
+    //    		'webkit-transform' : scstr
+    // 	});
+
+
+    // });
+
 
 
 
