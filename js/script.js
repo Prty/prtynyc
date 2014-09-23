da5c3483026913e389097610688146fdf9d1bbeb
tomgalle <admin@a23-0-3-10.deploy.static.akamaitechnologies.com>
2014-09-23 17:02:00 -0400

    version 02

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
-(function() {
+$(function(){
+
+    var windowheight = $(window).height();
+    var halfpage = windowheight / 2 - 91;
+    console.log(windowheight);
+
+    var top = $(".top");
+    var logo = $(".logo");
+    var arrow = $(".arrow")
+
+
+    top.css ({ height: windowheight });
+    logo.css({ "padding-top": halfpage });
+    // arrow.css({ bo: windowheight })
 
 
 
+    $(window).on("scroll", function(){
+
+    	var st = $(document).scrollTop();
+    	var sc = (-0.9/500) * st + 1
+    	var scstr = 'scale('+ sc + ')'
+    	console.log(sc)
+
+    	logo.css({
+    		// 'webkit-transform' : 'scale(0.5)'
+    		'webkit-transform' : scstr
+    	});
+
+
+    });
+
+
+ //   	console.log($(document).scrollTop());
+
+ //    $(window).on("scroll", function() {
+ //    	var sw = Math.min(322, $(document).scrollTop()) + 100;
+ //    	var sh = Math.min(95, $(document).scrollTop()) + 100;
+
+ //    	logo.width(sw).height(sh);
+	// });
 
 
 
-
-
-
-
-})();
+})
