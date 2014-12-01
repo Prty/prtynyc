80433930fc91ec857e71452622f74485bb805ca1
muroicci <eijimuroichi@Eijis-MacBook-Pro.local>
2014-12-01 11:59:36 -0500

    Turn off information when those values are empty

--- work.php
+++ work.php
@@ -16,12 +16,19 @@ Template Name: Archives
 		<a href="<?php the_permalink() ?>" data-background="<?php the_field('work_thumbnail', $id);?>">
 		<div>
 			<dl class="gravurthin">
+				<?php if(get_field('work_date')): ?>
 				<dt>Date</dt>
 				<dd><?php the_field('work_date', $id);?></dd>
+				<?php endif; ?>
+				<?php if(get_field('work_type')): ?>
 				<dt>Type</dt>
 				<dd><?php the_field('work_type', $id);?></dd>
+				<?php endif; ?>
+				<?php if(get_field('work_client')): ?>
 				<dt>Client</dt>
 				<dd><?php the_field('work_client', $id);?></dd>
+				<?php endif; ?>
+
 			</dl>
 			<h2 class="gravurthin">
 				<?php the_field('work_title', $id);?>
